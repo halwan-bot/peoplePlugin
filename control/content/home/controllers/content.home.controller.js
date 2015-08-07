014f4d6f84cb6c00c195eff01894d9e2e71724bd
vineetasharma <vineetasharma18791@gmail.com>
2015-08-07 16:40:25 +0530

    Export CSV file renamed to BuildFirePeoples.csv

--- control/content/home/controllers/content.home.controller.js
+++ control/content/home/controllers/content.home.controller.js
@@ -309,14 +309,14 @@
                         var csv = FormatConverter.JSON2CSV(json);
                         var blob = new Blob([csv], {type: 'text/csv;charset=utf-8;'});
                         if (navigator.msSaveBlob) {  // IE 10+
-                            navigator.msSaveBlob(blob, "Items.csv");
+                            navigator.msSaveBlob(blob, "BuildFirePeoples.csv");
                         }
                         else {
                             var link = document.createElement("a");
                             if (link.download !== undefined) {
                                 var url = URL.createObjectURL(blob);
                                 link.setAttribute("href", url);
-                                link.setAttribute("download", "MyBuildFireData.csv");
+                                link.setAttribute("download", "BuildFirePeoples.csv");
                                 link.style.visibility = 'hidden';
                                 document.body.appendChild(link);
                                 link.click();

ad2c2c1158742143c423e21fb2f09a6b4df8cba6
vineetasharma <vineetasharma18791@gmail.com>
2015-08-13 13:35:08 +0530

    #54 : Fixed export csv concating data issue

--- control/content/controllers/content.home.controller.js
+++ control/content/controllers/content.home.controller.js
@@ -383,6 +383,7 @@
                         else{
                             ContentHome.getTemplate();
                         }
+                        records=[];
                     });
                 };
                 /**

86a5c80cf6a8c6ceed94abf0e9613aa26814f88e
tabish.khan <tabish.khan@tothenew.com>
2015-10-13 21:21:03 +0530

    Modified the test cases for design section.

--- test/control/design/design.home.controller.spec.js
+++ test/control/design/design.home.controller.spec.js
@@ -1,13 +1,12 @@
 describe('Unit : peoplePluginDesign design.home.controller.js', function () {
-  var $scope, DesignHome, $rootScope, q, $controller, TAG_NAMES;
+  var $scope, DesignHome, $rootScope, q, $controller;
   beforeEach(module('peoplePluginDesign'));
 
-  beforeEach(inject(function (_$rootScope_, _$q_, _$controller_, _TAG_NAMES_) {
+  beforeEach(inject(function (_$rootScope_, _$q_, _$controller_) {
     $rootScope = _$rootScope_;
     q = _$q_;
     $scope = $rootScope.$new();
     $controller = _$controller_;
-    TAG_NAMES = _TAG_NAMES_;
   }));
 
   beforeEach(function () {

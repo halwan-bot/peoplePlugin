0d89fc1024dc11e7a7701b46034f57cac1a2ccab
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2015-10-21 11:07:19 +0530

    Fixed unsafe click issue

--- widget/app.js
+++ widget/app.js
@@ -29,7 +29,7 @@
       /**
        * To make href urls safe on mobile
        */
-      $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension|cdvfile):/);
+      $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension|cdvfile|file):/);
 
 
       $routeProvider

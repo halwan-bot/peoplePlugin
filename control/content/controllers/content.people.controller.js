0dafa9097ce8911bd6b92d1a4439c6aa2a597347
tabish.khan <tabish.khan@tothenew.com>
2015-10-23 17:30:24 +0530

    Fixed the issue [GAT-BFW006-3] [People Plugin] Link Disappears within an item when opened and cancelled (CustomerServiceTickets #323).

--- control/content/controllers/content.people.controller.js
+++ control/content/controllers/content.people.controller.js
@@ -140,6 +140,9 @@
             if (error) {
               return console.error('Error:', error);
             }
+            if (result === null) {
+              return console.error('Error:Can not save data, Null record found.');
+            }
             ContentPeople.item.data.socialLinks = ContentPeople.item.data.socialLinks || [];
             ContentPeople.item.data.socialLinks.splice(index, 1, result);
             $scope.$digest();
@@ -170,18 +173,27 @@
           }
         });
 
+        var linkOptions = {"icon": "true"};
+        ContentPeople.linksSortableOptions = {
+          handle: '> .cursor-grab'
+        };
+
         ContentPeople.openAddLinkPopup = function () {
           var options = {showIcons: false};
           var callback = function (error, result) {
             if (error) {
               return console.error('Error:', error);
             }
-            if (!ContentPeople.item.data.socialLinks)
+            if (!ContentPeople.item.data.socialLinks) {
               ContentPeople.item.data.socialLinks = [];
+            }
+            if (result === null) {
+              return console.error('Error:Can not save data, Null record found.');
+            }
             ContentPeople.item.data.socialLinks.push(result);
             $scope.$digest();
           };
-          Buildfire.actionItems.showDialog(null, options, callback);
+          Buildfire.actionItems.showDialog(null, linkOptions, callback);
         };
 
         ContentPeople.removeLink = function (_index) {

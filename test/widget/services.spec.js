22c0f65fa5f83c492e60a9b01a524fb9eeebcbf3
vineetasharma <vineetasharma18791@gmail.com>
2016-01-15 13:29:46 +0530

    Test cases added for services.js

--- test/widget/services.spec.js
+++ test/widget/services.spec.js
@@ -21,6 +21,45 @@ describe('Unit: peopleServices: Services', function () {
         datastore: {}
       };
     }));
-  })
+  });
+  describe('Unit : People service', function () {
+    var DB, People;
+    beforeEach(inject(
+        function (_DB_) {
+          DB = _DB_;
+          People = new DB('People');
+        }));
+    it('MediaContent should exists', function () {
+      expect(People).toBeDefined();
+      expect(People._tagName).toEqual('People');
+    });
+    it('MediaCenter methods should exists', function () {
+      expect(People.get).toBeDefined();
+      expect(People.find).toBeDefined();
+      expect(People.save).toBeDefined();
+      expect(People.update).toBeDefined();
+      expect(People.delete).toBeDefined();
+      expect(People.insert).toBeDefined();
+    });
+    it('MediaCenter methods should exists', function () {
+      Buildfire={datastore:{get:function(tagName,cb){
+        cb(null,{});
+      }}};
+      People.insert([]);
+      People.insert('asads');
+      People.getById('id1');
+      People.insert();
+      People.find();
+      People.find({});
+      People.update('id',{});
+      People.update();
+      People.update('id');
+      People.save({});
+      People.save();
+      People.delete();
+      People.delete('id');
+      People.clearListener();
+    });
+  });
 
 });

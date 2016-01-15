ce0901efb101b6884a4ffd1e7b22691a1a72390c
vineetasharma <vineetasharma18791@gmail.com>
2016-01-15 16:41:56 +0530

    Test cases added for design.app and content.services.js

--- test/control/design/app.spec.js
+++ test/control/design/app.spec.js
@@ -1,4 +1,4 @@
-xdescribe('Unit: peoplePluginDesign app', function () {
+describe('Unit: peoplePluginDesign app', function () {
   describe('Unit: app routes', function () {
     beforeEach(module('peoplePluginDesign'));
     var location, route, rootScope;
@@ -20,8 +20,34 @@ xdescribe('Unit: peoplePluginDesign app', function () {
       it('should load the home page on successful load of location path /', function () {
         location.path('/');
         rootScope.$digest();
-        expect(route.current.controller).toBe('DesignHomeCtrl')
+        expect(route.current.controller).toEqual('DesignHomeCtrl')
       });
     });
+  });
+});
+
+describe('Unit: peoplePluginDesign filter', function () {
+  beforeEach(module('peoplePluginDesign'));
+  var filter;
+  beforeEach(inject(function (_$filter_) {
+    filter = _$filter_;
+  }));
+
+  it('it should pass if "getImageUrl" filter returns resized image url', function () {
+    var result;
+    result = filter('getImageUrl')('https://imagelibserver.s3.amazonaws.com/25935164-2add-11e5-9d04-02f7ca55c361/950a50c0-400a-11e5-9af5-3f5e0d725ccb.jpg', 88, 124, 'resize');
+    expect(result).toEqual("http://s7obnu.cloudimage.io/s/resizenp/88x124/https://imagelibserver.s3.amazonaws.com/25935164-2add-11e5-9d04-02f7ca55c361/950a50c0-400a-11e5-9af5-3f5e0d725ccb.jpg");
+  });
+
+  it('it should give a default image even if parameter is blank', function () {
+    var result;
+    result = filter('getImageUrl')('', 88, 124, 'resize');
+    expect(result).toEqual("http://s7obnu.cloudimage.io/s/resizenp/88x124/");
+  });
+
+  it('it should pass if "getImageUrl" filter returns resized image url', function () {
+    var result;
+    result = filter('getImageUrl')('https://imagelibserver.s3.amazonaws.com/25935164-2add-11e5-9d04-02f7ca55c361/950a50c0-400a-11e5-9af5-3f5e0d725ccb.jpg', 88, 124, 'cropImage');
+    expect(result).toEqual("http://s7obnu.cloudimage.io/s/crop/88x124/https://imagelibserver.s3.amazonaws.com/25935164-2add-11e5-9d04-02f7ca55c361/950a50c0-400a-11e5-9af5-3f5e0d725ccb.jpg");
   });
 });

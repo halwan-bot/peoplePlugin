f897af19ba94145a9e7b441f577e6d2d1bcb2d34
Vishnu Singh <vish250491@gmail.com>
2016-02-23 15:24:48 +0530

    time related configuration to load lazy image done

--- widget/app.js
+++ widget/app.js
@@ -15,7 +15,7 @@
       .config(['lazyImgConfigProvider', function(lazyImgConfigProvider){
         // var scrollable = document.querySelector('#scrollable');
         lazyImgConfigProvider.setOptions({
-          offset: 30, // how early you want to load image (default = 100)
+          offset: 300, // how early you want to load image (default = 100)
           errorClass: 'error', // in case of loading image failure what class should be added (default = null)
           successClass: 'success', // in case of loading image success what class should be added (default = null)
           onError: function(image){

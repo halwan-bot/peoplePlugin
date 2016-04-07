617869d83c5aeb38ad83c56b4ff57a8d856587f8
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-04-07 13:03:30 +0530

    Removed old placeholder images. Replaced with new one and showed gif when loading images

--- widget/app.js
+++ widget/app.js
@@ -9,20 +9,8 @@
       'ngAnimate',
       'ngRoute',
       'ui.bootstrap',
-      'infinite-scroll',
-        'angularLazyImg'
+      'infinite-scroll'
     ])
-      .config(['lazyImgConfigProvider', function(lazyImgConfigProvider){
-        var scrollable = document.querySelector('#scrollable');
-        lazyImgConfigProvider.setOptions({
-          offset: 200, // how early you want to load image (default = 100)
-          errorClass: 'error', // in case of loading image failure what class should be added (default = null)
-          successClass: 'success', // in case of loading image success what class should be added (default = null)
-          onError: function(image){}, // function fired on loading error
-          onSuccess: function(image){}, // function fired on loading success
-          container: angular.element(scrollable) // if scrollable container is not $window then provide it here
-        });
-      }])
     .constant('TAG_NAMES', {
       PEOPLE_INFO: 'peopleInfo',
       PEOPLE: 'people'
@@ -46,7 +34,7 @@
 
       $routeProvider
         .when('/', {
-            template: '<div></div>'
+          template: '<div></div>'
         })
         .when('/people/:id', {
           templateUrl: 'templates/people.html',
@@ -92,23 +80,23 @@
       };
     }])
     .directive("loadImage", [function () {
-          return {
-              restrict: 'A',
-              link: function (scope, element, attrs) {
-                  element.attr("src", ".images/" + attrs.loadImage + ".png");
+      return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+          element.attr("src", "../../../styles/media/holder-" + attrs.loadImage + ".gif");
 
-                  var elem = $("<img>");
-                      elem[0].onload = function () {
-                          element.attr("src", attrs.finalSrc);
-                          elem.remove();
-                      };
-                      elem.attr("src", attrs.finalSrc);
-              }
+          var elem = $("<img>");
+          elem[0].onload = function () {
+            element.attr("src", attrs.finalSrc);
+            elem.remove();
           };
-      }])
-    .run(['Location', '$location','$rootScope', function (Location, $location,$rootScope) {
+          elem.attr("src", attrs.finalSrc);
+        }
+      };
+    }])
+    .run(['Location', '$location', '$rootScope', function (Location, $location, $rootScope) {
       buildfire.messaging.onReceivedMessage = function (msg) {
-        var currentUrl=$location.$$url;
+        var currentUrl = $location.$$url;
         switch (msg.type) {
           case 'AddNewItem':
             Location.goTo("#/people/" + msg.id + "?stopSwitch=true");
@@ -117,9 +105,9 @@
             Location.goTo("#/people/" + msg.id);
             break;
           default:
-              if(currentUrl!='/'){
-                  Location.goToHome();
-              }
+            if (currentUrl != '/') {
+              Location.goToHome();
+            }
         }
       };
       buildfire.deeplink.getData(function (data) {
@@ -134,20 +122,20 @@
           $rootScope.showHome = true;
           Location.goTo('#/');
         }
-        else{
-            buildfire.navigation._goBackOne();
+        else {
+          buildfire.navigation._goBackOne();
         }
       };
     }]).filter('cropImage', [function () {
-        return function (url, width, height, noDefault) {
-          if (noDefault) {
-            if (!url)
-              return '';
-          }
-          return buildfire.imageLib.cropImage(url, {
-            width: width,
-            height: height
-          });
-        };
-      }]);
+      return function (url, width, height, noDefault) {
+        if (noDefault) {
+          if (!url)
+            return '';
+        }
+        return buildfire.imageLib.cropImage(url, {
+          width: width,
+          height: height
+        });
+      };
+    }]);
 })(window.angular, window.buildfire);

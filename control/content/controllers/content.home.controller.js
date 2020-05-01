1211f7bbea5e8d97c717f8d91e7482fbdaec20df
Hanad Kubat <hanadkubat@Hanads-MacBook-Pro.local>
2020-05-01 17:56:01 +0200

    Fixed videos not showing when added from wysiwyg

--- control/content/controllers/content.home.controller.js
+++ control/content/controllers/content.home.controller.js
@@ -143,11 +143,19 @@
             return $sce.trustAsHtml(html);
         };
 
+        function myCustomURLConverter(url, node, on_save, name) {
+          if (!/^https?:\/\//i.test(url)) {
+            return "https://" + url.replace("//", "");
+          }
+          else return url;
+        }
+        
         ContentHome.descriptionWYSIWYGOptions = {
           plugins: 'advlist autolink link image lists charmap print preview',
           skin: 'lightgray',
           trusted: true,
           theme: 'modern',
+          urlconverter_callback : myCustomURLConverter,
           plugin_preview_width : "500",
           plugin_preview_height : "500"
         };
